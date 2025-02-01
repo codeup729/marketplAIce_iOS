5f8b17504b7df092ff971ff7c274ef02599d0be9
anitej-parametriks <anitejsri22@gmail.com>
2025-01-31 23:30:18 -0500

    Initial Commit

---
+++ marketplAIce_iOS/marketplAIce_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  marketplAIce_iOSApp.swift
+//  marketplAIce_iOS
+//
+//  Created by Anitej Srivastava on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct marketplAIce_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ marketplAIce_iOSTests/marketplAIce_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  marketplAIce_iOSTests.swift
+//  marketplAIce_iOSTests
+//
+//  Created by Anitej Srivastava on 31/01/25.
+//
+
+import Testing
+@testable import marketplAIce_iOS
+
+struct marketplAIce_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
